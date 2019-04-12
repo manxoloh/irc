c3f0cc58a95821d721ae3509d0b65112a7b95659
Solomon Maithya <maithyakavyu@outlook.com>
2019-04-12 11:00:09 -0400

    message

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/bootstrap-grid.css',
+        'css/icons.css',
+        'css/animate.min.css',
+        'css/style.css',
+        'css/responsive.css',
+        'css/chosen.css',
+        'css/colors/colors.css',
+        'css/bootstrap.css',
+        'css/font-awesome/4.5.0/css/font-awesome.min.css',
+    ];
+    public $js = [
+        'js/modernizr.js',
+        'js/script.js',
+        'js/bootstrap.min.js',
+        'js/wow.min.js',
+        'js/slick.min.js',
+        'js/parallax.js',
+        'js/select-chosen.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
